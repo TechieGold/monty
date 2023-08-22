1a3d41a83c1177dfd9e9d7a948349a1483504e73
Gold Israel <techiegold101@gmail.com>
2023-08-22 12:51:36 -0700

    check if arguments passed is 2

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include "monty.h"
 
+/**
+  * main - Entry point.
+  *
+  * Return: Always EXIT_SUCCESS.
+  */
 
-
-
-
-int main(int argc, char **argv);
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		printf("USAGE: %s\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
+}
