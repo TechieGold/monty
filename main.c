de45f1a128a4d0baa539678f32b16048828d6132
Gold Israel <techiegold101@gmail.com>
2023-08-22 13:05:00 -0700

    fix error

--- main.c
+++ main.c
@@ -11,13 +11,13 @@ int main(int argc, char **argv)
 	if (argc != 2)
 	{
 		printf("USAGE: %s\n", argv[0]);
-		return (EXIT_FAILURE);
+		exit (EXIT_FAILURE);
 	}
 
 	FILE *file = fopen(argv[1], "r");
 	if (!file)
 	{
 		printf("Can't open file %s\n", argv[1]);
-		return (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 }
